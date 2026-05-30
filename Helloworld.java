80f6e2429c0ea8a3cfbb9a211eb577c99d81054c
Umesh-666 <umeshshivapur373@gmail.com>
2026-05-30 11:04:14 +0530

    Fix case sensitivity and comment in HelloWorld

--- Helloworld.java
+++ Helloworld.java
@@ -3,7 +3,7 @@
   public static void main(string[] args){
    system.out.println("Hello, World");    //prints helloworld
 
-   system.out.println("Hello, World");
+   system.out.println("Hello, World"); //prints hellworld
     for(int i=0;i<5;i++)
       {
         System.out.println(i);
