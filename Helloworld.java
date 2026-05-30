20ff681683f080216a2027b0d4118c1fb22c23c0
Umesh-666 <umeshshivapur373@gmail.com>
2026-05-30 11:10:00 +0530

    Fix print statement and improve loop output

--- Helloworld.java
+++ Helloworld.java
@@ -6,6 +6,6 @@
    system.out.println("Hello, World"); //prints hellworldd
     for(int i=0;i<5;i++)
       {
-        System.out.println(i);
+        System.out.println(i); //print result
       }
 
