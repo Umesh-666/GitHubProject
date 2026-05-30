73d89630cb5ee17bcd469718004ca9b512af654d
Umesh-666 <umeshshivapur373@gmail.com>
2026-05-30 11:14:54 +0530

    Modify HelloWorld print statement
    Updated the print statement to include additional text.

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,7 @@
 #include<stdio.h>//UPDATED BY VIJAY
   public class HelloWorld {
   public static void main(string[] args){
-   system.out.println("Hello, World");    //prints helloworld
+   system.out.println("Hello, World");    //prints helloworldasdfghmme
 
    system.out.println("Hello, World");
     for(int i=0;i<5;i++)
